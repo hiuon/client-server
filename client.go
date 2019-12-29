123826220994131385d2310db5d351bf144e48b9
Vector <hiuon.x@gmail.com>
2019-12-29 23:52:08 +0300

    INIT

---
+++ client.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"net"
+	"os"
+	"strings"
+	"fmt"
+)
+
+const (
+	END_BYTES = "\000\001\002\003\004\005"
+	ADDR_SERVER = ":8080"
+)
+
+var name string = ""
+
+func main() {
+	fmt.Println("Enter your name: ")
+	fmt.Fscan(os.Stdin, &name)
+	fmt.Println("Welcome to chat, " + name + "\n///////////////\n")
+	conn, err := net.Dial("tcp", ADDR_SERVER)
+	if err != nil {
+		panic("can't connect to server")
+	}
+	defer conn.Close()
+	go ClientOutput(conn)
+	ClientInput(conn)
+}
+
+func ClientInput(conn net.Conn) {
+	for {
+		conn.Write([]byte(name + ": " + InputString() + END_BYTES))
+	}
+}
+
+func ClientOutput(conn net.Conn) {
+	var (
+			buffer = make([]byte, 512)
+			message string
+		)
+	close: for {
+		message = "" 
+		for {
+			length, err := conn.Read(buffer)
+			if err != nil {
+				break close
+			}
+			message += string(buffer[:length])
+			if strings.HasSuffix(message, END_BYTES) {
+				message = strings.TrimSuffix(message, END_BYTES)
+				break
+			}
+		}
+		fmt.Println(message)
+	}
+
+}
+
+func InputString() string {
+	msg, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	return strings.Replace(msg, "\n", "", -1)
+}
